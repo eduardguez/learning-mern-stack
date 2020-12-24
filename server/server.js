e5ab5d88822721b0bbbfcd90924f86a14aa74456
Eduardo Rdgz <e.peralta003@ebc.edu.mx>
2020-12-23 19:25:50 -0600

    coding LMS-Backend Configure mongodb connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,5 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const apis = require("./api");
 app.use("/api", apis);
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB Connection Succeeded!'))
+  .catch(error => console.log(error));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
