b7bf770cbb97c9b2238454a3590a087694c4fd88
Eduardo Rdgz <e.peralta003@ebc.edu.mx>
2020-12-23 20:07:38 -0600

    coding LMS-Backend Add routes to save and get weather from mongodb

--- server/api/index.js
+++ server/api/index.js
@@ -21,4 +21,23 @@ router.post("/weather", async (request, response) => {
   response.send(JSON.stringify(weatherData, null, 4));
 });
 
+router.post("/save_weather", async (request, response) => {
+  const { zipCode, tempMetric } = request.body;
+  let weather = new Weather();
+  let weatherData = await weather.getWeatherData(zipCode, tempMetric);
+
+  await weather.saveWeatherDataToMongo(zipCode, weatherData);
+  response.header("Content-Type", "application/json");
+  response.send(JSON.stringify(weatherData, null, 4));
+});
+
+router.get("/weather_by_zipcode", async (request, response) => {
+  const { zipCode } = request.query;
+  let weather = new Weather();
+
+  let weatherData = await weather.getWeatherDataFromMongo(zipCode);
+  response.header("Content-Type", "application/json");
+  response.send(JSON.stringify(weatherData, null, 4));
+});
+
 module.exports = router;
